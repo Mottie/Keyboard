fc2c39b46e4227dd2748ddcf2acd02481decd158
Rob Garrison <wowmotty@gmail.com>
2012-06-17 16:29:20 -0500

    fixed alwaysOpen focus issue & stayOpen not working - issue #69

--- js/jquery.keyboard.js
+++ js/jquery.keyboard.js
@@ -761,9 +761,10 @@ $.keyboard = function(el, options){
 	};
 
 	base.escClose = function(e){
-		if ( !base.isVisible ) { return; }
+		// keep keyboard open if alwaysOpen or stayOpen is true - fixes mutliple always open keyboards or single stay open keyboard
+		if ( !base.isVisible || (o.alwaysOpen && !base.isCurrent) || (!o.alwaysOpen && o.stayOpen && base.isCurrent) ) { return; }
+		if ( e.type === 'keyup' && e.which === 27 ) { return base.close(); }
 		// ignore autoaccept if using escape - good idea?
-		if ( e.type === 'keyup' && e.which === 27 ) { base.close(); }
 		if ( e.target !== base.el && $(e.target).closest('.ui-keyboard')[0] !== base.$keyboard[0] ) {
 			// stop propogation in IE - an input getting focus doesn't open a keyboard if one is already open
 			if ( base.allie ) {
