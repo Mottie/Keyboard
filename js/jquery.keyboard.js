77d08bf25e8625dd5ebc96218a7ccb376ae9254d
Rob Garrison <wowmotty@gmail.com>
2012-07-08 18:30:16 -0500

    fixed autoclosing keyboard

--- js/jquery.keyboard.js
+++ js/jquery.keyboard.js
@@ -405,8 +405,8 @@ $.keyboard = function(el, options){
 				if (base.checkCaret) { base.lastCaret = base.$preview.caret(); }
 			});
 			// prevent keyboard event bubbling
-			base.$keyboard.bind('mousedown.keyboard click.keyboard', function(){
-				return false;
+			base.$keyboard.bind('mousedown.keyboard click.keyboard', function(e){
+				e.stopPropagation();
 			});
 
 		// If preventing paste, block context menu (right click)
@@ -810,12 +810,14 @@ $.keyboard = function(el, options){
 	};
 
 	base.escClose = function(e){
-		if ( e.type === 'keyup' && e.which === 27 ) { return base.close(); }
-
+		if ( e.type === 'keyup' ) {
+			return ( e.which === 27 )  ? base.close() : '';
+		}
 		var cur = base.isCurrent();
 		// keep keyboard open if alwaysOpen or stayOpen is true - fixes mutliple always open keyboards or single stay open keyboard
 		if ( !base.isVisible || (o.alwaysOpen && !cur) || (!o.alwaysOpen && o.stayOpen && cur && !base.isVisible) ) { return; }
 		// ignore autoaccept if using escape - good idea?
+
 		if ( e.target !== base.el && cur ) {
 			// stop propogation in IE - an input getting focus doesn't open a keyboard if one is already open
 			if ( base.allie ) {
