ef832dc7dd67e935764b08ae3030261d0ccb64f7
José Miguel Sanz <jmsanzg@gmail.com>
2013-08-26 23:25:55 +0200

    #1 Navigation extension throws error when hiding keyboard
    When hidding the keyboard a new call is made to checkKeys with an undefined key. That's the way to detect the keyboard is or has been hidden

--- js/jquery.keyboard.extension-navigation.js
+++ js/jquery.keyboard.extension-navigation.js
@@ -79,7 +79,9 @@ $.fn.addNavigation = function(options){
 		};
 
 		base.checkKeys = function(key, disable){
-			var k = base.navigation_keys;
+                        if (key == undefined) {
+                            return;
+                        }			var k = base.navigation_keys;
 			if (key === k.toggle || disable) {
 				o.toggleMode = (disable) ? false : !o.toggleMode;
 				base.options.tabNavigation = (o.toggleMode) ? false : base.saveNav[0];
