25ec3bad670f8d44725721160a66feb8ffa680a1
Mottie <wowmotty@gmail.com>
2014-11-19 20:44:55 -0600

    Caret: detect selectionStart in Firefox using alternate method. Fixes #311

--- js/jquery.keyboard.js
+++ js/jquery.keyboard.js
@@ -172,7 +172,7 @@ $.keyboard = function(el, options){
 			setTimeout(function(){
 				// Number inputs don't support selectionStart and selectionEnd
 				// Number/email inputs don't support selectionStart and selectionEnd
-				if ( !/(number|email)/.test(base.el.type) ) {
+				if ( !/(number|email)/i.test(base.el.type) ) {
 					var c = base.$el.caret();
 					base.last.start = c.start;
 					base.last.end = c.end;
@@ -654,7 +654,7 @@ $.keyboard = function(el, options){
 			// using "kb" namespace for mouse repeat functionality to keep it separate
 			// I need to trigger a "repeater.keyboard" to make it work
 			.bind('mouseup.keyboard mouseleave.kb touchend.kb touchmove.kb touchcancel.kb', function(e){
-				if (/(mouseleave|touchend|touchcancel)/.test(e.type)) {
+				if (/(mouseleave|touchend|touchcancel)/i.test(e.type)) {
 					$(this).removeClass(o.css.buttonHover); // needed for touch devices
 				} else {
 					if (base.isVisible() && base.isCurrent()) { base.$preview.focus(); }
@@ -788,7 +788,7 @@ $.keyboard = function(el, options){
 		// check meta key set
 		if (base.metaActive) {
 			// the name attribute contains the meta set # "meta99"
-			key = (el && el.name && /meta/.test(el.name)) ? el.name : '';
+			key = (el && el.name && /meta/i.test(el.name)) ? el.name : '';
 			// save active meta keyset name
 			if (key === '') {
 				key = (base.metaActive === true) ? '' : base.metaActive;
@@ -1175,7 +1175,7 @@ $.keyboard = function(el, options){
 		$.each($.keyboard.layouts[o.layout], function(set, keySet){
 			var txt;
 			// skip layout name & lang settings
-			if (set !== "" && !/^(name|lang|rtl)$/.test(set)) {
+			if (set !== "" && !/^(name|lang|rtl)$/i.test(set)) {
 				// keep backwards compatibility for change from default to normal naming
 				if (set === 'default') { set = 'normal'; }
 				sets++;
@@ -1833,8 +1833,6 @@ $.keyboard = function(el, options){
  */
 (function($, len, createRange, duplicate){
 "use strict";
-// fix for IE8 (http://perrymitchell.net/article/ie8_javascript_indexof_hasownproperty/)
-window.hasOwnProperty = window.hasOwnProperty || Object.prototype.hasOwnProperty;
 
 $.fn.caret = function(options,opt2) {
 	if ( typeof this[0] === 'undefined' || this.is(':hidden') || this.css('visibility') === 'hidden' ) {
@@ -1844,7 +1842,7 @@ $.fn.caret = function(options,opt2) {
 		selection = document.selection, t = this[0], sTop = t.scrollTop,
 		ss = false, supportCaret = true;
 	try {
-		ss = t.hasOwnProperty('selectionStart');
+		ss = 'selectionStart' in t;
 	} catch(err){
 		supportCaret = false;
 	}
@@ -1856,7 +1854,7 @@ $.fn.caret = function(options,opt2) {
 		end = opt2;
 	}
 	if (supportCaret && typeof start !== 'undefined') {
-		if (!/(email|number)/.test(t.type)) {
+		if (!/(email|number)/i.test(t.type)) {
 			if (ss){
 				t.selectionStart=start;
 				t.selectionEnd=end;
@@ -1873,7 +1871,7 @@ $.fn.caret = function(options,opt2) {
 		t.scrollTop = sTop;
 		return this;
 	} else {
-		if (/(email|number)/.test(t.type)) {
+		if (/(email|number)/i.test(t.type)) {
 			// fix suggested by raduanastase (https://github.com/Mottie/Keyboard/issues/105#issuecomment-40456535)
 			s = e = this.val().length;
 		} else if (ss) {
