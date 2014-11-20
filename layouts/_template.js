066c4453143c882b4d8abcd6ee30e492ee82065a
Mottie <wowmotty@gmail.com>
2014-11-19 21:02:53 -0600

    Layouts: add Serbian & basic template layouts

---
+++ layouts/_template.js
@@ -0,0 +1,91 @@
+/* keyboard layout template (copy of "international" from the core plugin)
+ */
+
+jQuery.keyboard.layouts['international'] = {
+	'name' : 'international',
+	'lang' : ['all'], // this setting must match the "all" language definition below
+	'default': [
+		'` 1 2 3 4 5 6 7 8 9 0 - = {bksp}',
+		'{tab} q w e r t y u i o p [ ] \\',
+		'a s d f g h j k l ; \' {enter}',
+		'{shift} z x c v b n m , . / {shift}',
+		'{accept} {alt} {space} {alt} {cancel}'
+	],
+	'shift': [
+		'~ ! @ # $ % ^ & * ( ) _ + {bksp}',
+		'{tab} Q W E R T Y U I O P { } |',
+		'A S D F G H J K L : " {enter}',
+		'{shift} Z X C V B N M < > ? {shift}',
+		'{accept} {alt} {space} {alt} {cancel}'
+	],
+	'alt': [
+		'~ \u00a1 \u00b2 \u00b3 \u00a4 \u20ac \u00bc \u00bd \u00be \u2018 \u2019 \u00a5 \u00d7 {bksp}',
+		'{tab} \u00e4 \u00e5 \u00e9 \u00ae \u00fe \u00fc \u00fa \u00ed \u00f3 \u00f6 \u00ab \u00bb \u00ac',
+		'\u00e1 \u00df \u00f0 f g h j k \u00f8 \u00b6 \u00b4 {enter}',
+		'{shift} \u00e6 x \u00a9 v b \u00f1 \u00b5 \u00e7 > \u00bf {shift}',
+		'{accept} {alt} {space} {alt} {cancel}'
+	],
+	'alt-shift': [
+		'~ \u00b9 \u00b2 \u00b3 \u00a3 \u20ac \u00bc \u00bd \u00be \u2018 \u2019 \u00a5 \u00f7 {bksp}',
+		'{tab} \u00c4 \u00c5 \u00c9 \u00ae \u00de \u00dc \u00da \u00cd \u00d3 \u00d6 \u00ab \u00bb \u00a6',
+		'\u00c4 \u00a7 \u00d0 F G H J K \u00d8 \u00b0 \u00a8 {enter}',
+		'{shift} \u00c6 X \u00a2 V B \u00d1 \u00b5 \u00c7 . \u00bf {shift}',
+		'{accept} {alt} {space} {alt} {cancel}'
+	]
+};
+
+// Keyboard Language
+// please update this section to match this language and email me with corrections!
+// "all" is used here for example purposes, by convention it should be the ISO 639-1 code for the specified language
+// ***********************
+jQuery.keyboard.language.all = {
+	display : {
+		'a'      : '\u2714:Accept (Shift-Enter)', // check mark - same action as accept
+		'accept' : 'Accept:Accept (Shift-Enter)',
+		'alt'    : 'AltGr:Alternate Graphemes',
+		'b'      : '\u2190:Backspace',    // Left arrow (same as &larr;)
+		'bksp'   : 'Bksp:Backspace',
+		'c'      : '\u2716:Cancel (Esc)', // big X, close - same action as cancel
+		'cancel' : 'Cancel:Cancel (Esc)',
+		'clear'  : 'C:Clear',             // clear num pad
+		'combo'  : '\u00f6:Toggle Combo Keys',
+		'dec'    : '.:Decimal',           // decimal point for num pad (optional), change '.' to ',' for European format
+		'e'      : '\u21b5:Enter',        // down, then left arrow - enter symbol
+		'enter'  : 'Enter:Enter',
+		'lock'   : '\u21ea Lock:Caps Lock', // caps lock
+		's'      : '\u21e7:Shift',        // thick hollow up arrow
+		'shift'  : 'Shift:Shift',
+		'sign'   : '\u00b1:Change Sign',  // +/- sign for num pad
+		'space'  : '&nbsp;:Space',
+		't'      : '\u21e5:Tab',          // right arrow to bar (used since this virtual keyboard works with one directional tabs)
+		'tab'    : '\u21e5 Tab:Tab'       // \u21b9 is the true tab symbol (left & right arrows)
+	},
+	// Message added to the key title while hovering, if the mousewheel plugin exists
+	wheelMessage : 'Use mousewheel to see other keys'
+
+	// uncomment, then include changes to the comboRegex here
+	/*
+	, comboRegex : /([`\'~\^\"ao])([a-z])/mig,
+	*/
+
+	// uncomment, then include any changes to the combos option here
+	/*
+	, combos : {
+		// grave
+		'`' : { a:"\u00e0", A:"\u00c0", e:"\u00e8", E:"\u00c8", i:"\u00ec", I:"\u00cc", o:"\u00f2", O:"\u00d2",
+				u:"\u00f9", U:"\u00d9", y:"\u1ef3", Y:"\u1ef2" },
+		// acute & cedilla
+		"'" : { a:"\u00e1", A:"\u00c1", e:"\u00e9", E:"\u00c9", i:"\u00ed", I:"\u00cd", o:"\u00f3", O:"\u00d3",
+				u:"\u00fa", U:"\u00da", y:"\u00fd", Y:"\u00dd" },
+		// umlaut/trema
+		'"' : { a:"\u00e4", A:"\u00c4", e:"\u00eb", E:"\u00cb", i:"\u00ef", I:"\u00cf", o:"\u00f6", O:"\u00d6",
+				u:"\u00fc", U:"\u00dc", y:"\u00ff", Y:"\u0178" },
+		// circumflex
+		'^' : { a:"\u00e2", A:"\u00c2", e:"\u00ea", E:"\u00ca", i:"\u00ee", I:"\u00ce", o:"\u00f4", O:"\u00d4",
+				u:"\u00fb", U:"\u00db", y:"\u0177", Y:"\u0176" },
+		// tilde
+		'~' : { a:"\u00e3", A:"\u00c3", e:"\u1ebd", E:"\u1ebc", i:"\u0129", I:"\u0128", o:"\u00f5", O:"\u00d5",
+				u:"\u0169", U:"\u0168", y:"\u1ef9", Y:"\u1ef8", n:"\u00f1", N:"\u00d1" }
+	}
+	*/
+};
---
+++ layouts/serbian.js
@@ -0,0 +1,161 @@
+/* Serbian keyboard layouts
+ * contains layout: 'sr-Cyrl' & 'sr'
+ *
+ * To use:
+ *  Point to this js file into your page header: <script src="layouts/russian.js" type="text/javascript"></script>
+ *  Initialize the keyboard using: $('input').keyboard({ layout: 'russian-qwerty' });
+ *
+ * license for this file: WTFPL, unless the source layout site has a problem with me using them as a reference
+ */
+
+/* Serbian Cyrillic Keyboard Layout (српски) * generated from http://www.greywyvern.com/code/javascript/keyboard layouts */
+$.keyboard.layouts["sr-Cyrl"] = {
+	'name' : "Serbian Cyrillic (sr-Cyrl)",
+	'lang' : ['sr'],
+	"default" : [
+		"` 1 2 3 4 5 6 7 8 9 0 ' + {bksp}",
+		"{tab} \u0459 \u045a \u0435 \u0440 \u0442 \u0437 \u0443 \u0438 \u043e \u043f \u0448 \u0452 \u0436",
+		" \u0430 \u0441 \u0434 \u0444 \u0433 \u0445 \u0458 \u043a \u043b \u0447 \u045b {enter}",
+		"{shift} < \u0455 \u045f \u0446 \u0432 \u0431 \u043d \u043c , . - {shift}",
+		"{accept} {alt} {space} {alt} {cancel}"
+	],
+	'shift' : [
+		'~ ! " # $ % & / ( ) = ? * {bksp}',
+		"{tab} \u0409 \u040a \u0415 \u0420 \u0422 \u0417 \u0423 \u0418 \u041e \u041f \u0428 \u0402 \u0416",
+		" \u0410 \u0421 \u0414 \u0424 \u0413 \u0425 \u0408 \u041a \u041b \u0427 \u040b {enter}",
+		"{shift} > \u0405 \u040f \u0426 \u0412 \u0411 \u041d \u041c ; : _ {shift}",
+		"{accept} {alt} {space} {alt} {cancel}"
+	],
+	'alt' : [
+		"` 1 2 3 4 5 6 7 8 9 0 ' + {bksp}",
+		"{tab} \u0459 \u045a \u0435 \u0440 \u0442 \u0437 \u0443 \u0438 \u043e \u043f \u0448 \u0452 \u0436",
+		" \u0430 \u0441 \u0434 \u0444 \u0433 \u0445 \u0458 \u043a \u043b \u0447 \u045b {enter}",
+		"{shift} < \u0455 \u045f \u0446 \u0432 \u0431 \u043d \u043c , . - {shift}",
+		"{accept} {alt} {space} {alt} {cancel}"
+	]
+};
+
+/* Serbian Latin Keyboard Layout (Srpski) * generated from http://www.greywyvern.com/code/javascript/keyboard layouts */
+$.keyboard.layouts.sr = {
+	'name' : "Serbian Latin (sr)",
+	'lang' : ['sr'],
+	"default" : [
+		"\u00b8 1 2 3 4 5 6 7 8 9 0 ' + {bksp}",
+		"{tab} q w e r t z u i o p \u0161 \u0111 \u017e",
+		" a s d f g h j k l \u010d \u0107 {enter}",
+		"{shift} < y x c v b n m , . - {shift}",
+		"{accept} {alt} {space} {alt} {cancel}"
+	],
+	shift : [
+		'\u00a8 ! " # $ % & / ( ) = ? * {bksp}',
+		"{tab} Q W E R T Z U I O P \u0160 \u0110 \u017d",
+		" A S D F G H J K L \u010c \u0106 {enter}",
+		"{shift} > Y X C V B N M ; : _ {shift}",
+		"{accept} {alt} {space} {alt} {cancel}"
+	],
+	alt : [
+		"\u00b8 1 2 3 4 5 6 7 8 9 0 ' + {bksp}",
+		"{tab} q w e r t z u i o p \u0161 \u0111 \u017e",
+		" a s d f g h j k l \u010d \u0107 {enter}",
+		"{shift} < y x c v b n m , . - {shift}",
+		"{accept} {alt} {space} {alt} {cancel}"
+	]
+};
+
+/* Serbian Cyrillic Keyboard Layout (српски) for Mac */
+$.keyboard.layouts["sr-Cyrl-mac"] = {
+	'name' : "Serbian Cyrillic for Mac (sr-Cyrl)",
+	'lang' : ['sr'],
+	"default" : [
+		"< 1 2 3 4 5 6 7 8 9 0 ' + {bksp}",
+		"{tab} \u0459 \u045a \u0435 \u0440 \u0442 \u0437 \u0443 \u0438 \u043e \u043f \u0448 \u0452 \u0436",
+		" \u0430 \u0441 \u0434 \u0444 \u0433 \u0445 \u0458 \u043a \u043b \u0447 \u045b {enter}",
+		"{shift} \u0455 \u045f \u0446 \u0432 \u0431 \u043d \u043c , . - {shift}",
+		"{accept} {alt} {space} {alt} {cancel}"
+	],
+	shift : [
+		'> ! " # $ % & / ( ) = ? * {bksp}',
+		"{tab} \u0409 \u040a \u0415 \u0420 \u0422 \u0417 \u0423 \u0418 \u041e \u041f \u0428 \u0402 \u0416",
+		" \u0410 \u0421 \u0414 \u0424 \u0413 \u0425 \u0408 \u041a \u041b \u0427 \u040b {enter}",
+		"{shift} \u0405 \u040f \u0426 \u0412 \u0411 \u041d \u041c ; : _ {shift}",
+		"{accept} {alt} {space} {alt} {cancel}"
+	],
+	alt : [
+		"` \u2020 \u00b0 \u20ac \u201e \u2122 \u221e \u00a3 \u0490 \u00b6 \u2022 \u2013 = {bksp}",
+		"{tab} q w e r t y u i o p [ ] \u2018",
+		' a s d f g h j k l ; " {enter}',
+		"{shift} z x c v b n m , . / {shift}",
+		"{accept} {alt} {space} {alt} {cancel}"
+	],
+	"alt-shift" : [
+		"~ ! @ # \u201c % ^ & * ( ) \u2014 + {bksp}",
+		"{tab} Q W E R T Y U I O P { } \u2019",
+		" A S D F G H J K L : \u201d {enter}",
+		"{shift} Z X C V B N M < > ? {shift}",
+		"{accept} {alt} {space} {alt} {cancel}"
+	]
+};
+
+/* Serbian Latin Keyboard Layout (Srpski) for Mac */
+$.keyboard.layouts["sr-mac"] = {
+	'name' : "Serbian Latin for Mac (sr)",
+	'lang' : ['sr'],
+	"default" : [
+		"< 1 2 3 4 5 6 7 8 9 0 / + {bksp}",
+		"{tab} q w e r t z u i o p \u0161 \u0111 \u017e",
+		" a s d f g h j k l \u010d \u0107 {enter}",
+		"{shift} y x c v b n m , . - {shift}",
+		"{accept} {alt} {space} {alt} {cancel}"
+	],
+	shift : [
+		"> ! \" # $ % & ' ( ) = ? * {bksp}",
+		"{tab} Q W E R T Z U I O P \u0160 \u0110 \u017d",
+		" A S D F G H J K L \u010c \u0106 {enter}",
+		"{shift} Y X C V B N M ; : _ {shift}",
+		"{accept} {alt} {space} {alt} {cancel}"
+	],
+	alt : [
+		"` \u00a1 \u2122 \u00a3 \u00a2 \u221e \u00a7 \u00b6 \u2022 \u00aa \u00ba - \u2260 {bksp}",
+		"{tab} \u0153 \u2211 \u00b4 \u00ae \u2020 \u2014 \u00a8 ^ \u00f8 \u03c0 [ ]  ",
+		"\u00e5 \u00df \u2202 \u0192 \u00a9  \u030f \u2206  \u0311 \u00ac \u2026 ^ {enter}",
+		"{shift} \u03a9 \u2248 \u00e7 \u221a \u222b ~ \u00b5 \u2264 \u2265 \u00f7 {shift}",
+		"{accept} {alt} {space} {alt} {cancel}"
+	],
+	"alt-shift" : [
+		"~ \u2044 @ \u2039 \u203a \u20ac \u0131 \u2013 \u00b0 \u00b7 \u201a _ \u00b1 {bksp}",
+		"{tab} \u0152 \u201e \u2030 \u201c \u2018 \u201d \u2019 \u00c9 \u00d8 \u220f { } |",
+		"\u00c5 \u00cd \u00ce \u00cf \u00cc \u00d3 \u00d4 \uf8ff \u00d2 \u00e6 \u00c6 {enter}",
+		"{shift} \u00ab \u00bb \u00c7 \u25ca \u00d1 \u02c6 \u00af \u00c8 \u02c7 \u00bf {shift}",
+		"{accept} {alt} {space} {alt} {cancel}"
+	]
+};
+
+// Keyboard Language
+// please update this section to match this language and email me with corrections!
+// sr = ISO 639-1 code for Serbian
+// ***********************
+jQuery.keyboard.language.sr = {
+	display : {
+		'a'      : '\u2714:Accept (Shift-Enter)', // check mark - same action as accept
+		'accept' : 'Accept:Accept (Shift-Enter)',
+		'alt'    : 'AltGr:Alternate Graphemes',
+		'b'      : '\u2190:Backspace',    // Left arrow (same as &larr;)
+		'bksp'   : 'Bksp:Backspace',
+		'c'      : '\u2716:Cancel (Esc)', // big X, close - same action as cancel
+		'cancel' : 'Cancel:Cancel (Esc)',
+		'clear'  : 'C:Clear',             // clear num pad
+		'combo'  : '\u00f6:Toggle Combo Keys',
+		'dec'    : ',:Decimal',           // decimal point for num pad (optional), change '.' to ',' for European format
+		'e'      : '\u21b5:Enter',        // down, then left arrow - enter symbol
+		'enter'  : 'Enter:Enter',
+		'lock'   : '\u21ea Lock:Caps Lock', // caps lock
+		's'      : '\u21e7:Shift',        // thick hollow up arrow
+		'shift'  : 'Shift:Shift',
+		'sign'   : '\u00b1:Change Sign',  // +/- sign for num pad
+		'space'  : '&nbsp;:Space',
+		't'      : '\u21e5:Tab',          // right arrow to bar (used since this virtual keyboard works with one directional tabs)
+		'tab'    : '\u21e5 Tab:Tab'       // \u21b9 is the true tab symbol (left & right arrows)
+	},
+	// Message added to the key title while hovering, if the mousewheel plugin exists
+	wheelMessage : 'Use mousewheel to see other keys',
+};
