3ca454bf1909e637cc0a53bba5d948f1cafa7fd7
Mottie <wowmotty@gmail.com>
2015-07-07 07:27:23 -0500

    internal version bump

--- js/jquery.keyboard.js
+++ js/jquery.keyboard.js
@@ -1,4 +1,4 @@
-/*! jQuery UI Virtual Keyboard v1.23.2 *//*
+/*! jQuery UI Virtual Keyboard v1.23.3 *//*
 
 Author: Jeremy Satterfield
 Modified: Rob Garrison (Mottie on github)
@@ -40,7 +40,7 @@ Setup/Usage:
 var $keyboard = $.keyboard = function(el, options){
 	var base = this, o;
 
-	base.version = '1.23.2';
+	base.version = '1.23.3';
 
 	// Access to jQuery and DOM versions of element
 	base.$el = $(el);
