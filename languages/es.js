2575bd3690c07759446abf3ef79fe6db7d37de59
Adrian Fish <adrian.r.fish@gmail.com>
2015-07-08 16:56:55 +0100

    Fixes #367. Translated keys into spanish

---
+++ languages/es.js
@@ -0,0 +1,29 @@
+// Keyboard Language
+// please update this section to match this language and email me with corrections!
+// es = ISO 639-1 code for Spanish
+// ***********************
+jQuery.keyboard.language.es = {
+	language: 'Spanish',
+	display : {
+		'a'      : '\u2714:Acceptar (Cambio+Inscribir)', // check mark - same action as accept
+		'accept' : 'Acceptar:Acceptar (Cambio+Inscribir)',
+		'alt'    : 'AltGr:Grafemas Alternativos',
+		'b'      : '\u2190:Retroceso',    // Left arrow (same as &larr;)
+		'bksp'   : 'Bksp:Retroceso',
+		'c'      : '\u2716:Cancelar (Esc)', // big X, close - same action as cancel
+		'cancel' : 'Cancelar:Cancelar (Esc)',
+		'clear'  : 'C:Vaciar',             // clear num pad
+		'combo'  : '\u00f6:Alternar las Teclas Combinados',
+		'dec'    : ',:Decimal',           // decimal point for num pad (optional), change '.' to ',' for European format
+		'e'      : '\u21b5:Inscribir',        // down, then left arrow - enter symbol
+		'enter'  : 'Inscribir:Inscribir',
+		'lock'   : '\u21ea Bloq:Mayús', // caps lock
+		's'      : '\u21e7:Cambio',        // thick hollow up arrow
+		'shift'  : 'Cambio:Cambio',
+		'sign'   : '\u00b1:Cambiar Signo',  // +/- sign for num pad
+		'space'  : '&nbsp;:Espacio',
+		't'      : '\u21e5:Tab',          // right arrow to bar (used since this virtual keyboard works with one directional tabs)
+		'tab'    : '\u21e5 Tab:Tab'       // \u21b9 is the true tab symbol (left & right arrows)
+	},
+	wheelMessage : 'Utilice la rueda del mouse para ver otras teclas'
+};
