f62197427eb3b4f03104d225cd332239ac5f9bfd
Rob Garrison <wowmotty@gmail.com>
2017-02-03 19:39:06 -0600

    Core: Fix empty openOn. See #519

--- js/jquery.keyboard.js
+++ js/jquery.keyboard.js
@@ -1650,7 +1650,7 @@ http://www.opensource.org/licenses/mit-license.php
 		if ($target.hasClass(kbcss.input)) {
 			var kb = $target.data('keyboard');
 			// only trigger on self
-			if (kb !== base && !kb.$el.hasClass(kbcss.isCurrent)) {
+			if (kb !== base && !kb.$el.hasClass(kbcss.isCurrent) && kb.options.openOn) {
 				kb.focusOn();
 			}
 		}
