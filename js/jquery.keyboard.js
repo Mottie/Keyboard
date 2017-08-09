0cf8be57b1aca06038ce0363b215d7c7e26efa9d
Rob Garrison <wowmotty@gmail.com>
2017-08-09 07:27:36 -0500

    Core: Restore minimum jQuery to v1.4.3. Fixes #586

--- js/jquery.keyboard.js
+++ js/jquery.keyboard.js
@@ -219,14 +219,16 @@ http://www.opensource.org/licenses/mit-license.php
 		var $toggle = base.$keyboard.find('.' + $keyboard.css.keyToggle),
 			locked = !base.enabled;
 		// prevent physical keyboard from working
-		base.$preview.prop('readonly', locked || base.options.lockInput);
+		base.preview.readonly = locked || base.options.lockInput;
 		// disable all buttons
 		base.$keyboard
 			.toggleClass($keyboard.css.keyDisabled, locked)
 			.find('.' + $keyboard.css.keyButton)
 			.not($toggle)
-			.prop('disabled', locked)
-			.attr('aria-disabled', locked);
+			.attr('aria-disabled', locked)
+			.each(function() {
+				this.disabled = locked;
+			});
 		$toggle.toggleClass($keyboard.css.keyDisabled, locked);
 		// stop auto typing
 		if (locked && base.typing_options) {
