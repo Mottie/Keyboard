241fb19a159b5b58730c738e12a19faa26054a5f
Durga Madhab <durga598@gmail.com>
2019-07-15 13:35:36 +0530

    Update ms-Hindi.js
    added ya and yya

--- layouts/ms-Hindi.js
+++ layouts/ms-Hindi.js
@@ -8,21 +8,21 @@ jQuery.keyboard.layouts['ms-Hindi Traditional'] = {
 		"{empty} 1 2 3 4 5 6 7 8 9 0 - \u0943 {bksp}",
 		"{tab} \u094c \u0948 \u093e \u0940 \u0942 \u092c \u0939 \u0917 \u0926 \u091c \u0921 \u093c \u0949",
 		"\u094b \u0947 \u094d \u093f \u0941 \u092a \u0930 \u0915 \u0924 \u091a \u091f {enter}",
-		"{shift} \u0949 \u0902 \u092e \u0928 \u0935 \u0932 \u0938 , . / {shift}",
+		"{shift} \u0949 \u0902 \u092e \u0928 \u0935 \u0932 \u0938 , . \u092f {shift}",
 		"{accept} {alt} {space} {alt} {cancel}"
 	],
 	"shift" : [
 		"{empty} \u090d \u0945 \u094d\u0930 \u0930\u094d \u091c\u094d\u091e \u0924\u094d\u0930 \u0915\u094d\u0937 \u0936\u094d\u0930 ( ) \u0903 \u090b {bksp}",
 		"{tab} \u0914 \u0910 \u0906 \u0908 \u090a \u092d \u0919 \u0918 \u0927 \u091d \u0922 \u091e \u0911",
 		"\u0913 \u090f \u0905 \u0907 \u0909 \u092b \u0931 \u0916 \u0925 \u091b \u0920 {enter}",
-		"{shift} \u0911 \u0901 \u0923 {empty} {empty} \u0933 \u0936 \u0937 \u0964 / {shift}",
+		"{shift} \u0911 \u0901 \u0923 {empty} {empty} \u0933 \u0936 \u0937 \u0964 \u095f {shift}",
 		"{accept} {alt} {space} {alt} {cancel}"
 	],
 	"alt" : [
 		"` \u0967 \u0968 \u0969 \u096a \u096b \u096c \u096d \u096e \u096f \u0966 - = {bksp}",
 		"{tab} {empty} {empty} {empty} {empty} {empty} {empty} {empty} {empty} {empty} {empty} [ ] \\",
 		"{empty} {empty} {empty} {empty} {empty} {empty} {empty} {empty} {empty} ; ' {enter}",
-		"{shift} {empty} {empty} {empty} {empty} {empty} {empty} {empty} , . {empty} {shift}",
+		"{shift} {empty} {empty} {empty} {empty} {empty} {empty} {empty} , . / {shift}",
 		"{accept} {alt} {space} {alt} {cancel}"
 	],
 	"alt-shift" : [
